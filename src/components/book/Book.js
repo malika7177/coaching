a55af25f6eecdb2692d552be4d0bc3f3eb0b9b86
malika7177 <malikadjrv@gmail.com>
2024-07-25 00:07:24 +0500

    book update

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import "./book.css";
+import { Link } from "react-router-dom";
+import { SiGooglemeet } from "react-icons/si";
 
 const Book = () => {
   return (
-    <div>
-      <h1>Book</h1>
-    </div>
-  )
-}
+    <section className="book container">
+      <div className="book-container">
+        <div className="book-info">
+          <h1>
+            Get 15-Minutes Complimentary <br /> online session.
+          </h1>
+          <p>Limited Period Offer. Claim Now.</p>
+          <Link to="/" className="btn">
+            Book Now
+          </Link>
+        </div>
+        <div className="book-img">
+          <img src="/assets/book.jpg" alt="book" />
+          <div className="book-icon">
+            <SiGooglemeet />
+            meet
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Book
+export default Book;
