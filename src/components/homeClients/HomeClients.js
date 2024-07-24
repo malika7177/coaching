8661770a04196fbf2c7508c99deac18f3edb86f8
malika7177 <malikadjrv@gmail.com>
2024-07-24 21:58:07 +0500

    checking in

--- src/components/homeClients/HomeClients.js
+++ src/components/homeClients/HomeClients.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import "./homeClients.css";
+import { IoStarOutline } from "react-icons/io5";
 
 const HomeClients = () => {
   return (
-    <div>
-      <h1>Clients</h1>
-    </div>
-  )
-}
+    <section className="hm-clients container">
+      <h1>
+        Hear out what my <br /> clients say about me.
+      </h1>
+      <div className="hm-clients_cards">
+        <div className="hm-clients_card">
+          <p>
+            "I gained so much confidence in my ability to connect and deepen my
+            relationships with people. "
+          </p>
+          <h3>Jane</h3>
+          <img src="/assets/person1.png" />
+          <IoStarOutline className="star" /> <IoStarOutline className="star" />{" "}
+          <IoStarOutline className="star" /> <IoStarOutline className="star" />{" "}
+          <IoStarOutline className="star" />
+        </div>
+        <div className="hm-clients_card">
+          <p>
+            "I gained so much confidence in my ability to connect and deepen my
+            relationships with people. "
+          </p>
+          <h3>Jane</h3>
+          <img src="/assets/person1.png" />
+          <IoStarOutline className="star" /> <IoStarOutline className="star" />{" "}
+          <IoStarOutline className="star" /> <IoStarOutline className="star" />{" "}
+          <IoStarOutline className="star" />
+        </div>
+        <div className="hm-clients_card">
+          <p>
+            "I gained so much confidence in my ability to connect and deepen my
+            relationships with people. "
+          </p>
+          <h3>Jane</h3>
+          <img src="/assets/person1.png" />
+          <IoStarOutline className="star" /> <IoStarOutline className="star" />{" "}
+          <IoStarOutline className="star" /> <IoStarOutline className="star" />{" "}
+          <IoStarOutline className="star" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default HomeClients
+export default HomeClients;
