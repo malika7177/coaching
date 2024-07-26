61853c025e9c23ff5802c52b9b69fe804f33e7fc
malika7177 <malikadjrv@gmail.com>
2024-07-26 22:49:07 +0500

    testimonials update

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -1,11 +1,99 @@
-import React from 'react'
+import React from "react";
+import "./testimonials.css";
+import { Link } from "react-router-dom";
+import { FaStar } from "react-icons/fa";
+
 
 const Testimonials = () => {
   return (
-    <section className='testimonials'>
-      Testimonials
+    <section className="testimonials">
+      <div className="tes-info">
+        <div className="tes-intro">
+          <p>Testimonials</p>
+          <h1>Results that speak for themselves</h1>
+        </div>
+        <div className="tes-btn">
+          <Link to="/" className="btn">
+            Get started
+          </Link>
+        </div>
+      </div>
+      <div className="tes-cards">
+        <div className="tes-card">
+          <p>
+            Lorem ipsum dolor sit amet,
+            <span>consectetur adipiscing elit, sed do eiusmod tempor incididunt.</span>
+             Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit.
+          </p>
+          <img src="/assets/person1.png" alt="person" />
+          <h3>Naam</h3>
+          <h5>Bedrijf</h5>
+          <div className="stars">
+          <FaStar className="star" />
+            <FaStar className="star" />
+            <FaStar className="star" />
+            <FaStar className="star" />
+            <FaStar className="star" />
+          </div>
+        </div>
+        <div className="tes-card">
+          <p>
+            Lorem ipsum dolor sit amet,
+            <span>consectetur adipiscing elit, sed do</span>
+            eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit.
+          </p>
+          <img src="/assets/person1.png" alt="person" />
+          <h3>Naam</h3>
+          <h5>Bedrijf</h5>
+          <div className="stars">
+          <FaStar className="star" />
+            <FaStar className="star" />
+            <FaStar className="star" />
+            <FaStar className="star" />
+            <FaStar className="star" />
+          </div>
+        </div>
+        <div className="tes-card">
+          <p>
+            Lorem ipsum dolor sit amet,
+            <span>consectetur adipiscing elit, sed do</span>
+            eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit.
+          </p>
+          <img src="/assets/person1.png" alt="person" />
+          <h3>Naam</h3>
+          <h5>Bedrijf</h5>
+          <div className="stars">
+          <FaStar className="star" />
+            <FaStar className="star" />
+            <FaStar className="star" />
+            <FaStar className="star" />
+            <FaStar className="star" />
+          </div>
+        </div>
+        <div className="tes-card">
+          <p>
+            Lorem ipsum dolor sit amet,
+            <span>consectetur adipiscing elit, sed do</span>
+            eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit.
+          </p>
+          <img src="/assets/person1.png" alt="person" />
+          <h3>Naam</h3>
+          <h5>Bedrijf</h5>
+          <div className="stars">
+          <FaStar className="star" />
+            <FaStar className="star" />
+            <FaStar className="star" />
+            <FaStar className="star" />
+            <FaStar className="star" />
+          </div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonials;
