ff2e36ab6c79ba5c6e76a93060458404ff1e09c3
malika7177 <malikadjrv@gmail.com>
2024-07-27 17:50:18 +0500

    companies update

--- src/components/companies/Companies.js
+++ src/components/companies/Companies.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import "./companies.css";
+import { Link } from "react-router-dom";
 
 const Companies = () => {
   return (
-    <div>
-      companies
-    </div>
-  )
-}
+    <section className="companies">
+      <p>From startups to the world’s largest companies</p>
+      <div className="companies-imgs">
+        <Link to="/">
+          <img src="/assets/company1.png" alt="logo1" />
+        </Link>
+        <Link to="/">
+          <img src="/assets/company2.png" alt="logo2" />
+        </Link>
+        <Link to="/">
+          <img src="/assets/company3.png" alt="logo3" />
+        </Link>
+        <Link to="/">
+          <img src="/assets/company4.png" alt="logo4" />
+        </Link>
+        <Link to="/">
+          <img src="/assets/company5.png" alt="logo5" />
+        </Link>
+      </div>
+    </section>
+  );
+};
 
-export default Companies
+export default Companies;
