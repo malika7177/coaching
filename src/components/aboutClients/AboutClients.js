8785c28cd12a7727b337622dec2f66ff10652d12
malika7177 <malikadjrv@gmail.com>
2024-07-28 15:03:02 +0500

    clients update

--- src/components/aboutClients/AboutClients.js
+++ src/components/aboutClients/AboutClients.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import "./aboutClients.css";
 
 const AboutClients = () => {
   return (
-    <div>
-      clients
-    </div>
-  )
-}
+    <section className="about-clients">
+      <h1>
+        Hear out what my <br />
+        clients say about me.
+      </h1>
+      <div className="ac-cards">
+        <div className="ac-card">
+          <h2>5/5</h2>
+          <p>
+            "I gained so much <br /> <span>confidence</span> in my ability to connect and deepen my
+            relationships with people. "
+          </p>
+          <h3>Jane</h3>
+          <div className="ac-card_img">
+            <img src="/assets/person3.png" alt="person" />
+          </div>
+        </div>
+        <div className="ac-card">
+          <h2>5/5</h2>
+          <p>
+            “Denise helped me <span>organise</span> my diet. Highly recommend her services!”
+          </p>
+          <h3>Catherine</h3>
+          <div className="ac-card_img">
+            <img src="/assets/person2.png" alt="person" />
+          </div>
+        </div>
+        <div className="ac-card">
+          <h2>5/5</h2>
+          <p>
+            "I gained so much <br /> <span>confidence</span> in my ability to connect and deepen my
+            relationships with people. "
+          </p>
+          <h3>Jane</h3>
+          <div className="ac-card_img">
+            <img src="/assets/person3.png" alt="person" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default AboutClients
+export default AboutClients;
