58639806b7059109da2e56ec1a54fbc767b7ae44
malika7177 <malikadjrv@gmail.com>
2024-07-30 17:46:20 +0500

    update

--- src/components/sHelp/SHelp.js
+++ src/components/sHelp/SHelp.js
@@ -1,11 +1,75 @@
-import React from 'react'
+import React from "react";
+import "./sHelp.css";
+import { RiBookletLine } from "react-icons/ri";
+import { IoMdChatbubbles } from "react-icons/io";
+import { BiMaleFemale } from "react-icons/bi";
 
 const SHelp = () => {
   return (
-    <section className='s-help'>
-      help
-    </section>
-  )
-}
+    <section className="s-help">
+      <div className="s-help_info">
+        <h1>
+          I can help you in these <br /> particular areas
+        </h1>
+      </div>
+      <div className="s-help_container">
 
-export default SHelp
+      <div className="s-help_content">
+
+        <div className="s-help_box">
+          <div className="s-help_icons">
+            <BiMaleFemale className="s-help_icon" />
+          </div>
+          <h2>Group Coaching Sessions</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua minim
+            veniam.
+          </p>
+        </div>
+
+        <div className="s-help_box">
+          <div className="s-help_icons">
+            <IoMdChatbubbles className="s-help_icon" />
+          </div>
+          <h2>Consultation</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua minim
+            veniam.
+          </p>
+        </div>
+      </div>
+
+
+      <div className="s-help_content2">
+        <div className="s-help_box">
+          <div className="s-help_icons">
+            <RiBookletLine className="s-help_icon" />
+          </div>
+          <h2>Online course</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua minim
+            veniam.
+          </p>
+        </div>
+
+        <div className="s-help_box boxx">
+          <div className="s-help_icons">
+            <RiBookletLine className="s-help_icon" />
+          </div>
+          <h2>1:1 Coaching</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua minim
+            veniam.
+          </p>
+        </div>
+      </div>
+      </div>
+    </section>
+  );
+};
+
+export default SHelp;
