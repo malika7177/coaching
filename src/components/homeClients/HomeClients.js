e295c2aa5e1008337be97716fea4bb984058591a
malika7177 <malikadjrv@gmail.com>
2024-08-13 16:30:30 +0500

    update

--- src/components/homeClients/HomeClients.js
+++ src/components/homeClients/HomeClients.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./homeClients.css";
 import { FaStar } from "react-icons/fa";
 import HomeClientsData from "../../data/HomeClientsData";
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Autoplay } from "swiper/modules";
 
 const HomeClients = () => {
   return (
@@ -9,10 +13,19 @@ const HomeClients = () => {
       <h1>
         Hear out what my <br /> clients say about me.
       </h1>
-      <div className="hm-clients_cards">
+      <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay]}
+        className="hm-clients_cards"
+      >
         {HomeClientsData.map((item) => {
           return (
-            <div className="hm-clients_card" key={item.id}>
+            <SwiperSlide className="hm-clients_card" key={item.id}>
               <p>{item.comment}</p>
               <h3>{item.name}</h3>
               <img src={item.img} alt="person" />
@@ -23,10 +36,10 @@ const HomeClients = () => {
                 <FaStar className="star" />
                 <FaStar className="star" />
               </div>
-            </div>
+            </SwiperSlide>
           );
         })}
-      </div>
+      </Swiper>
     </section>
   );
 };
--- src/data/HomeClientsData.js
+++ src/data/HomeClientsData.js
@@ -1,23 +1,47 @@
 const HomeClientsData = [
-    {
-        id: 1,
-        comment: 'I gained so much confidence in my ability to connect and deepen my relationships with people." ',
-        name: 'Angelina',
-        img: '/assets/person3.png',
-    },
-    {
-        id: 2,
-        comment: 'I gained so much  in my ability to connect and deepen my relationships with people." ',
-        name: 'Jane',
-        img: '/assets/person1.png',
-    },
-    {
-        id: 3,
-        comment: 'I gained so much  in my ability to connect and deepen my relationships with people." ',
-        name: 'Catherine',
-        img: '/assets/person2.png',
-    },
-   
-]
+  {
+    id: 1,
+    comment:
+      'I gained so much confidence in my ability to connect and deepen my relationships with people." ',
+    name: "Angelina",
+    img: "/assets/person3.png",
+  },
+  {
+    id: 2,
+    comment:
+      'I gained so much  in my ability to connect and deepen my relationships with people." ',
+    name: "Jane",
+    img: "/assets/person1.png",
+  },
+  {
+    id: 3,
+    comment:
+      'I gained so much  in my ability to connect and deepen my relationships with people." ',
+    name: "Catherine",
+    img: "/assets/person2.png",
+  },
+
+  {
+    id: 4,
+    comment:
+      'I gained so much confidence in my ability to connect and deepen my relationships with people." ',
+    name: "Angelina",
+    img: "/assets/person3.png",
+  },
+  {
+    id: 5,
+    comment:
+      'I gained so much  in my ability to connect and deepen my relationships with people." ',
+    name: "Jane",
+    img: "/assets/person1.png",
+  },
+  {
+    id: 6,
+    comment:
+      'I gained so much  in my ability to connect and deepen my relationships with people." ',
+    name: "Catherine",
+    img: "/assets/person2.png",
+  },
+];
 
 export default HomeClientsData;
