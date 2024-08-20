bd31ced2722ccc4d25887df55b93378d4e459fa6
malika7177 <163645394+malika7177@users.noreply.github.com>
2024-08-20 17:50:45 +0500

    Update GuideHero.js

--- src/components/guideHero/GuideHero.js
+++ src/components/guideHero/GuideHero.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./guideHero.css";
 import { Link } from "react-router-dom";
 import { RiChat1Line } from "react-icons/ri";
-
+import Aos from "aos";
+import 'aos/dist/aos.css';
 
 const GuideHero = () => {
+   useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
   return (
     <section className="guide-hero">
-      <div className="gh-content">
+      <div className="gh-content"  data-aos="fade-right">
         <h1>Results that speak for themselves</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
